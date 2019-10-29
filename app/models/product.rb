b98f4676ab6f3f950691baa3a452aef509653560
Ionar Rafael <irm@ionar.com.br>
2019-10-29 14:31:37 -0300

    change selectbox text for ordered and multicolumn labeled

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
 	has_many :samplings, dependent: :destroy
-    validates :name, :peso_minimo, :peso_maximo, :peso_liquido, :preco, presence: true
+  validates :name, :peso_minimo, :peso_maximo, :peso_liquido, presence: true
+
+  default_scope { order(name: :asc) }
+
+  def product_label
+    "#{self.name.to_s} - min: #{self.peso_minimo}g | max: #{self.peso_maximo}g"
+  end
 end
