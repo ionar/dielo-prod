f724bfe886ccde41bb5d0403aa069faf35c54d7d
Ionar Rafael <irm@ionar.com.br>
2019-10-29 17:58:34 -0300

    fix order error

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	has_many :samplings, dependent: :destroy
   validates :name, :peso_minimo, :peso_maximo, :peso_liquido, presence: true
 
-  default_scope { order(name: :asc) }
+  scope :name_ordened, -> { order(name: :asc) }
 
   def product_label
     "#{self.name.to_s} - min: #{self.peso_minimo}g | max: #{self.peso_maximo}g"
